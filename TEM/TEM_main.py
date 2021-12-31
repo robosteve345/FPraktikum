2bf3402ceff503913a00290f2db97c6fb8903d25
LukasK <lukaskoenig2410@gmail,com>
2021-12-31 12:36:14 +0100

    Slightly prettier draft with higher resolution

--- TEM/TEM_main.py
+++ TEM/TEM_main.py
@@ -2,12 +2,18 @@ import numpy as np
 from matplotlib import pyplot as plt
 from scipy import signal
 from scipy.ndimage import gaussian_filter1d
+from scipy import interpolate
 
-x, intensity = np.genfromtxt("02122021goldkrass.txt", skip_header=1, usecols=(0,1), unpack=True)
+x, intensity = np.genfromtxt("02122021goldkrass.txt", skip_header=1, usecols=(0,1), unpack=True) # read stuff
+
+x_new = np.linspace(min(x), max(x), 200)                    # make stuff more finely grained
+intensity_new = interpolate.interp1d(x, intensity)(x_new)   # by linear interpolation
+ysmoothed = gaussian_filter1d(intensity_new, sigma=1)       # make graph smooth by gaussian filter
+peaks = signal.find_peaks(ysmoothed, x_new)                 # find peaks of smooth stuff
+
+# plot stuff
 plt.fill_between(x, intensity, 350, alpha=0.7)
-ysmoothed = gaussian_filter1d(intensity, sigma=1)
-plt.plot(x, ysmoothed)
-peaks = signal.find_peaks(ysmoothed, x)
+plt.plot(x_new, ysmoothed)
 for i in peaks[0]:
-    plt.axvline(x[i])
+    plt.axvline(x_new[i])
 plt.show()
